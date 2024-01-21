653a494c89a312cc26a6d3eafe5dfeb7d02cfdb0
Guilherme Kilian <guilherme.kilian@umbler.com>
2024-01-21 16:22:16 -0300

    create account

--- src/Auth/Rpg.Account/Models/ApplicationUser.cs
+++ src/Auth/Rpg.Account/Models/ApplicationUser.cs
@@ -4,4 +4,5 @@ namespace Rpg.Account.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    public string Name { get; set; }
 }
---
+++ src/Auth/Rpg.Account/Pages/Account/Create/Index.cshtml.cs
@@ -0,0 +1,128 @@
+using Duende.IdentityServer;
+using Duende.IdentityServer.Events;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using Duende.IdentityServer.Stores;
+using Duende.IdentityServer.Test;
+using IdentityServer.Pages.Create;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Rpg.Account.Models;
+
+namespace Rpg.Account.Pages.Account.Create;
+
+[SecurityHeaders]
+[AllowAnonymous]
+public class Index : PageModel
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IIdentityServerInteractionService _interaction;
+    private readonly IEventService _events;
+    private readonly IAuthenticationSchemeProvider _schemeProvider;
+    private readonly IIdentityProviderStore _identityProviderStore;
+
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    public Index(
+        IIdentityServerInteractionService interaction,
+        IAuthenticationSchemeProvider schemeProvider,
+        IIdentityProviderStore identityProviderStore,
+        IEventService events,
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _interaction = interaction;
+        _schemeProvider = schemeProvider;
+        _identityProviderStore = identityProviderStore;
+        _events = events;
+    }
+
+    public IActionResult OnGet(string returnUrl)
+    {
+        Input = new InputModel { ReturnUrl = returnUrl };
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        // check if we are in the context of an authorization request
+        var context = await _interaction.GetAuthorizationContextAsync(Input.ReturnUrl);
+
+        // the user clicked the "cancel" button
+        if (Input.Button != "create")
+            if (context != null)
+            {
+                // if the user cancels, send a result back into IdentityServer as if they 
+                // denied the consent (even if this client does not require consent).
+                // this will send back an access denied OIDC error response to the client.
+                await _interaction.DenyAuthorizationAsync(context, AuthorizationError.AccessDenied);
+
+                // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
+                if (context.IsNativeClient())
+                    // The client is native, so this change in how to
+                    // return the response is for better UX for the end user.
+                    return this.LoadingPage(Input.ReturnUrl);
+
+                return Redirect(Input.ReturnUrl);
+            }
+            else
+                // since we don't have a valid context, then we just go back to the home page
+                return Redirect("~/");
+
+        if (_userManager.FindByNameAsync(Input.Username) != null)
+            ModelState.AddModelError("Input.Username", "Invalid username");
+
+        if (ModelState.IsValid)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = Input.Username,
+                Email = Input.Email,
+                Name = Input.Name,
+
+            };
+
+            var result = await _userManager.CreateAsync(user, Input.Password);
+
+            if (result.Succeeded)
+            {
+                await _events.RaiseAsync(new UserLoginSuccessEvent(user.Email, user.Id.ToString(), user.Email, clientId: context?.Client.ClientId));
+
+                await _signInManager.SignInAsync(user, new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    IsPersistent = true,
+                    ExpiresUtc = DateTime.UtcNow.AddDays(7)
+                });
+
+                // request for a local page
+                if (Url.IsLocalUrl(Input.ReturnUrl))
+                    return Redirect(Input.ReturnUrl);
+                else if (string.IsNullOrEmpty(Input.ReturnUrl))
+                    return Redirect("~/");
+                else
+                    // user might have clicked on a malicious link - should be logged
+                    throw new Exception("invalid return URL");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    if (error.Code is "DuplicateUserName" or "DuplicateEmail")
+                        ModelState.AddModelError("Email", "Este email já está sendo utilizado. Tente outro");
+                    else
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+        }
+
+        return Page();
+    }
+}
